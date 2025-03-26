65ca1e1d79b2c3d64851de4ac559db97bfdbf87e
clintjeff2 <clintjeff2@gmail.com>
2025-03-26 20:44:43 +0100

    Created the event-gallery entity

---
+++ src/event-gallery/entities/event-gallery.entity.ts
@@ -0,0 +1,32 @@
+import { 
+  Entity, 
+  PrimaryGeneratedColumn, 
+  Column, 
+  ManyToOne, 
+  JoinColumn 
+} from 'typeorm';
+import { Event } from '../../events/entities/event.entity';
+
+@Entity()
+export class EventGallery {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  imageUrl: string;
+
+  @Column()
+  description: string;
+
+  @ManyToOne(() => Event, event => event.eventGallery, { 
+    onDelete: 'CASCADE' 
+  })
+  @JoinColumn({ name: 'eventId' })
+  event: Event;
+
+  @Column()
+  eventId: string;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+}
