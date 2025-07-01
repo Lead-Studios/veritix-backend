c5ffae0a8d5687d9d6fde716103db29a5d6a0720
LaGodxy <algodicino@gmail.com>
2025-07-01 15:09:06 -0700

    Implement Admin Event APIs

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -7,10 +7,15 @@ import { CampaignEmailService } from './services/campaign-email.service';
 import { DashboardController } from './controllers/dashboard.controller';
 import { TicketController } from './controllers/ticket.controller';
 import { CampaignEmailController } from './controllers/campaign-email.controller';
+import { Event } from '../event/entities/event.entity';
+import { EventService } from './services/event.service';
+import { ReportService } from './services/report.service';
+import { EventController } from './controllers/event.controller';
+import { ReportController } from './controllers/report.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CampaignEmail])],
-  providers: [DashboardService, TicketService, CampaignEmailService],
-  controllers: [DashboardController, TicketController, CampaignEmailController],
+  imports: [TypeOrmModule.forFeature([CampaignEmail, Event])],
+  providers: [DashboardService, TicketService, CampaignEmailService, EventService, ReportService],
+  controllers: [DashboardController, TicketController, CampaignEmailController, EventController, ReportController],
 })
 export class AdminModule {} 
--- src/admin/services/dashboard.service.ts
+++ src/admin/services/dashboard.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class DashboardService {
   async getAnalytics() {
-    // TODO: Implement real analytics logic
     return {
       users: 1000,
       tickets: 500,
