c7d549e3e014593d40074a17160f63982b61d34e
LaGodxy <algodicino@gmail.com>
2025-07-05 16:42:14 -0700

    Build Conference Analytics Dashboard for Organizers

--- src/analytics/analytics.module.ts
+++ src/analytics/analytics.module.ts
@@ -3,30 +3,49 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TicketHistory } from '../ticket/entities/ticket-history.entity';
 import { Ticket } from '../ticketing/entities/ticket.entity';
 import { Conference } from '../conference/entities/conference.entity';
+import { Session } from './entities/session.entity';
+import { Attendance } from './entities/attendance.entity';
+import { Feedback } from './entities/feedback.entity';
 import { EventAnalyticsService } from './services/event-analytics.service';
 import { EventAnalyticsController } from './controllers/event-analytics.controller';
 import { ConferenceTicketAnalyticsService } from './services/conference-ticket-analytics.service';
 import { ConferenceTicketAnalyticsController } from './controllers/conference-ticket-analytics.controller';
+import { SessionAnalyticsService } from './services/session-analytics.service';
+import { SessionAnalyticsController } from './controllers/session-analytics.controller';
+import { AnalyticsService } from './services/analytics.service';
+import { AnalyticsController } from './controllers/analytics.controller';
+import { ExportService } from './services/export.service';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([
       TicketHistory, 
       Ticket, 
-      Conference
+      Conference,
+      Session,
+      Attendance,
+      Feedback
     ])
   ],
   providers: [
     EventAnalyticsService,
-    ConferenceTicketAnalyticsService
+    ConferenceTicketAnalyticsService,
+    SessionAnalyticsService,
+    AnalyticsService,
+    ExportService
   ],
   controllers: [
     EventAnalyticsController,
-    ConferenceTicketAnalyticsController
+    ConferenceTicketAnalyticsController,
+    SessionAnalyticsController,
+    AnalyticsController
   ],
   exports: [
     EventAnalyticsService,
-    ConferenceTicketAnalyticsService
+    ConferenceTicketAnalyticsService,
+    SessionAnalyticsService,
+    AnalyticsService,
+    ExportService
   ],
 })
 export class AnalyticsModule {} 
