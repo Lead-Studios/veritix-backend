307ab5de5fe004dc4a8ef92aafb7d3d02650648f
LaGodxy <algodicino@gmail.com>
2025-07-30 12:31:38 -0700

    Ticket Insurance API Integration

--- src/tickets/tickets.module.ts
+++ src/tickets/tickets.module.ts
@@ -2,10 +2,19 @@ import { Module } from '@nestjs/common';
 import { TicketsController } from './tickets.controller';
 import { TicketsService } from './tickets.service';
 import { PaymentService } from './payment.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TicketingTicket } from '../ticketing/entities/ticket.entity';
+import { TicketingEvent } from '../ticketing/entities/event.entity';
+import { InsuranceService } from './services/insurance.service';
+import { NftTicketsModule } from '../nft-tickets/nft-tickets.module';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([TicketingTicket, TicketingEvent]),
+    NftTicketsModule, // Import NftTicketsModule as TicketsService depends on NftTicketsService
+  ],
   controllers: [TicketsController],
-  providers: [TicketsService, PaymentService],
+  providers: [TicketsService, PaymentService, InsuranceService],
   exports: [TicketsService],
 })
 export class TicketsModule {} 
