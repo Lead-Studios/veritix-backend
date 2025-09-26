eb081636bb92d953ab84786440aa6bc176113c39
GideonBature <infoaboutgideon@gmail.com>
2025-09-26 13:20:09 +0100

    Provide APIs for organizers to monitor ticket sales and attendees

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OrganizerController } from './organizer/organizer.controller';
+import { OrganizerService } from './organizer/organizer.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, OrganizerController],
+  providers: [AppService, OrganizerService],
 })
 export class AppModule {}
